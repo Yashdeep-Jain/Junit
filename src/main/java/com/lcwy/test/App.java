c8da3708627c38f9f9aa7188b78d1a0028e4b437
Yashdeep Jain <yashuofficial7@gmail.com>
2024-05-27 01:03:57 +0530

    Base structure added

---
+++ src/main/java/com/lcwy/test/App.java
@@ -0,0 +1,13 @@
+package com.lcwy.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
